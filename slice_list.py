dd4c0ee6113119d2939b5fd784bc4569a851e5d8
Alona Iavorska <alyona3jaworska@gmail.com>
2021-11-20 20:44:32 -0500

    Create slice_list.py

---
+++ slice_list.py
@@ -0,0 +1,27 @@
+# Your code below:
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+prices = [2, 6, 1, 3, 2, 7, 2]
+num_two_dollar_slices = prices.count(2)
+num_pizzas=len(toppings)
+
+pizza_and_prices= [[2, "pepperoni"], [6, "pineapple"], [1, "cheese"], [3, "sausage"], [2, "olives"], [7, "anchovies"], [2, "mushrooms"]]
+
+print(toppings)
+print(prices)
+print(num_two_dollar_slices)
+print(num_pizzas)
+print("We sell", num_pizzas, "different kinds of pizza!")
+print(pizza_and_prices)
+pizza_and_prices.sort()
+print(pizza_and_prices)
+cheapest_pizza = pizza_and_prices[0]
+print(cheapest_pizza)
+priciest_pizza = pizza_and_prices[-1]
+print(priciest_pizza)
+pizza_and_prices.pop()
+print(pizza_and_prices)
+pizza_and_prices.insert(4, [2.5, "peppers"])
+print(pizza_and_prices)
+three_cheapest = pizza_and_prices[:3]
+print(three_cheapest)
+
